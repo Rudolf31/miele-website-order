93d33cbe69868a8e64b4f1518fc1ce5a70d72949
Rudolf31 <rudolfshevchenko@gmail.com>
2025-03-26 16:56:15 +0300

    now has been created new page - catalog

---
+++ src/app/catalog/page.js
@@ -0,0 +1,14 @@
+import Header from "@/app/components/Header/Header";
+import Footer from "@/app/components/Footer/Footer";
+import ContactFloatingPanel from "@/app/components/ContactFloatingPanel/ContactFloatingPanel";
+
+ export default function Catalog() {
+
+    return (
+        <>
+            <Header/>
+            <Footer/>
+            <ContactFloatingPanel/>
+        </>
+    )
+ }
